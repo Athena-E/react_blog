a611851f3d33f5e1c11531e6bfe1c158d8cc677d
Athena-E <91805494+Athena-E@users.noreply.github.com>
2023-09-05 20:21:42 +0100

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// async redirects() {
-//   return [
-//     {
-//       source: "/",
-//       destination: "/home",
-//       permanent: true,
-//     },
-//   ];
-// },
-
 module.exports = nextConfig;
